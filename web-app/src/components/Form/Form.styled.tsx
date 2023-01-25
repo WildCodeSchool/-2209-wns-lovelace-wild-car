57e4284b7c66553c27ddf2b419510fec56101d97
Valentin Coppin <valentincoppin@hotmail.fr>
2023-01-25 11:32:32 +0100

    working on form

--- web-app/src/components/Form/Form.styled.tsx
+++ web-app/src/components/Form/Form.styled.tsx
@@ -1,7 +1,16 @@
 import styled from 'styled-components';
 
 export const SForm = styled.div`
-  border: 1px solid green;
+  /* border: 1px solid green; */
+  display: flex;
+  justify-content: center;
+
+  .container {
+    background-color: grey;
+    width: 280px;
+    height: 240px;
+    border-radius: 20px;
+  }
 
   .block {
     display: flex;
--- web-app/src/components/Form/Form.tsx
+++ web-app/src/components/Form/Form.tsx
@@ -3,16 +3,18 @@ import { SForm } from './Form.styled';
 const Form = () => {
   return (
     <SForm>
-      <h1>départ</h1>
-      <textarea></textarea>
-      <h1>arrivée</h1>
-      <textarea />
-      <div className='block'>
-        <h1>calendrier</h1>
-        <h1>nb place</h1>
-      </div>
-      <div className='go'>
-        <button>go !</button>
+      <div className='container'>
+        <h1>départ</h1>
+        <textarea></textarea>
+        <h1>arrivée</h1>
+        <textarea />
+        <div className='block'>
+          <h1>calendrier</h1>
+          <h1>nb place</h1>
+        </div>
+        <div className='go'>
+          <button>go !</button>
+        </div>
       </div>
     </SForm>
   );
