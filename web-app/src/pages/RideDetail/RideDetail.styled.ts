926b6caaf280b81a037a66adfd01598b20bf4c8d
Valentin Coppin <valentincoppin@hotmail.fr>
2023-01-26 17:33:08 +0100

    start the ride detail page

---
+++ web-app/src/pages/RideDetail/RideDetail.styled.ts
@@ -0,0 +1,53 @@
+import styled from 'styled-components';
+
+export const DetailSection = styled.section`
+  height: 50vh;
+  width: 100vw;
+  background-color: white;
+`;
+export const RideDate = styled.h1`
+  color: black;
+`;
+export const Start = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+export const StartTime = styled.h1`
+  color: black;
+`;
+export const StartCity = styled.h1`
+  color: black;
+`;
+export const End = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+export const EndTime = styled.h1`
+  color: black;
+`;
+export const EndCity = styled.h1`
+  color: black;
+`;
+export const Price = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+export const PriceH1 = styled.h1`
+  color: orange;
+`;
+export const PriceValue = styled.h1`
+  color: orange;
+`;
+export const Driver = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+export const DriverName = styled.h1``;
+
+export const InfoSection = styled.section`
+  height: 50vh;
+  width: 100vw;
+  background-color: #f87a23;
+`;
--- web-app/src/pages/RideDetail/RideDetail.tsx
+++ web-app/src/pages/RideDetail/RideDetail.tsx
@@ -1,12 +1,70 @@
+import ChooseRideButton from '../../components/ChooseRideButton/ChooseRideButton';
+import RideCard from '../../components/RideCard/RideCard';
+import {
+  Dot,
+  DotContainer,
+  DriverImage,
+  DriverPP,
+  DriverPPImage,
+  EndingPointImage,
+  StartingPointImage,
+} from '../../components/RideCard/RideCard.styled';
+import {
+  Start,
+  DetailSection,
+  InfoSection,
+  RideDate,
+  StartTime,
+  StartCity,
+  End,
+  EndTime,
+  EndCity,
+  Price,
+  Driver,
+  DriverName,
+  PriceH1,
+  PriceValue,
+} from './RideDetail.styled';
+import startingPoint from '../../img/starting-point.png';
+import endingPoint from '../../img/ending-point.png';
+import steeringWheel from '../../img/steering-wheel.png';
+import driver from '../../img/driver.png';
+
 const RideDetail = () => {
   return (
     <>
-      <section className='detail'>
-        <h1>ride detail</h1>
-      </section>
-      <section className='info'>
-        <h1>info</h1>
-      </section>
+      <DetailSection>
+        <RideDate>date du trajet</RideDate>
+        <Start>
+          <StartTime>heure de départ</StartTime>
+          <StartingPointImage src={startingPoint} alt='start position' />
+          <StartCity>ville de départ</StartCity>
+        </Start>
+        <DotContainer>
+          <Dot />
+          <Dot />
+          <Dot />
+        </DotContainer>
+        <End>
+          <EndTime>heure d'arrivée</EndTime>
+          <EndingPointImage src={endingPoint} alt='ending position' />
+          <EndCity>ville d'arrivée</EndCity>
+        </End>
+        <Price>
+          <PriceH1>prix</PriceH1>
+          <PriceValue>€</PriceValue>
+        </Price>
+        <Driver>
+          <DriverImage src={steeringWheel} alt='steering wheel' />
+          <DriverName>nom</DriverName>
+          <DriverPP>
+            <DriverPPImage src={driver} alt='jean' />
+          </DriverPP>
+        </Driver>
+      </DetailSection>
+      <InfoSection>
+        <ChooseRideButton />
+      </InfoSection>
     </>
   );
 };
