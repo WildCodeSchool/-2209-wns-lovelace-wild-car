0aa34eb16a3c8e8042a5330ef39ec8acc8bd7854
CLément <clmentmoreno@hotmail.com>
2023-03-02 14:37:39 +0100

    commit for changing branch

--- WildCarMobile/components/RideCardMobile.js
+++ WildCarMobile/components/RideCardMobile.js
@@ -1,76 +1,80 @@
 import React from 'react';
-import { View, Image, Text, StyleSheet, Button } from 'react-native';
+import { View, Image, Text, StyleSheet, TouchableOpacity } from 'react-native';
 
 const RideCardMobile = () => {
   return (
     <View style={styles.container}>
-      <View style={styles.leftSide}>
-        <View style={styles.distanceBetweenPoint}>
-          <View style={styles.StartingPoint}>
+      <View style={styles.top}>
+        <View style={styles.leftSide}>
+          <View style={styles.distanceBetweenPoint}>
+            <View style={styles.StartingPoint}>
+              <Image
+                style={styles.image}
+                source={require('../assets/starting-point.png')}
+                alt="start position"
+              />
+              <Text style={styles.texte}>lyon</Text>
+            </View>
+            <View style={styles.dotContainer}>
+              <View style={styles.dot} />
+              <View style={styles.dot} />
+              <View style={styles.dot} />
+            </View>
+            <View style={styles.EndingPoint}>
+              <Image
+                style={styles.image}
+                source={require('../assets/ending-point.png')}
+                alt="ending position"
+              />
+              <Text style={styles.texte}>paris</Text>
+            </View>
+          </View>
+          <View style={styles.Driver}>
             <Image
               style={styles.image}
-              source={require('../assets/starting-point.png')}
-              alt="start position"
+              source={require('../assets/steering-wheel.png')}
+              alt="steering wheel"
             />
-            <Text style={styles.texte}>lyon</Text>
+            <Text style={styles.texte}>Dave Lopper</Text>
           </View>
-          <View style={styles.dotContainer}>
-            <View style={styles.dot} />
-            <View style={styles.dot} />
-            <View style={styles.dot} />
-          </View>
-          <View style={styles.EndingPoint}>
+          <View style={styles.Passengers}>
             <Image
               style={styles.image}
-              source={require('../assets/ending-point.png')}
-              alt="ending position"
+              source={require('../assets/passengers.png')}
+              alt="passengers"
             />
-            <Text style={styles.texte}>paris</Text>
+            <Text style={styles.texte}>3</Text>
           </View>
         </View>
-        <View style={styles.Driver}>
-          <Image
-            style={styles.image}
-            source={require('../assets/steering-wheel.png')}
-            alt="steering wheel"
-          />
-          <Text style={styles.texte}>Dave Lopper</Text>
-        </View>
-        <View style={styles.Passengers}>
-          <Image
-            style={styles.image}
-            source={require('../assets/passengers.png')}
-            alt="passengers"
-          />
-          <Text style={styles.texte}>3</Text>
-        </View>
-      </View>
-      <View>
-        <View style={styles.Time}>
-          <Image
-            style={styles.image}
-            source={require('../assets/time.png')}
-            alt="time"
-          />
-          <Text style={styles.texte}>08h45</Text>
-        </View>
-        <View style={styles.Calendar}>
-          <Image
-            style={styles.image}
-            source={require('../assets/calendar.png')}
-            alt="calendar"
-          />
-          <Text style={styles.texte}>jeu 15</Text>
-        </View>
-        <View>
-          <Image
-            style={styles.image}
-            source={require('../assets/driver.png')}
-            alt="jean"
-          />
+        <View style={styles.rightSide}>
+          <View style={styles.Time}>
+            <Image
+              style={styles.image}
+              source={require('../assets/time.png')}
+              alt="time"
+            />
+            <Text style={styles.texte}>08h45</Text>
+          </View>
+          <View style={styles.Calendar}>
+            <Image
+              style={styles.image}
+              source={require('../assets/calendar.png')}
+              alt="calendar"
+            />
+            <Text style={styles.texte}>jeu 15</Text>
+          </View>
+          <View>
+            <Image
+              style={styles.driverImage}
+              source={require('../assets/driver.png')}
+              alt="jean"
+            />
+          </View>
         </View>
       </View>
-      {/*   <Button style={styles.button} title="détails du trajet" />*/}
+      <TouchableOpacity activeOpacity={1} style={styles.button}>
+        <Text style={{ color: 'white' }}>Réserver votre trajet</Text>
+      </TouchableOpacity>
     </View>
   );
 };
@@ -86,8 +90,8 @@ const styles = StyleSheet.create({
     backgroundColor: 'white',
     flex: 0.7,
     alignItems: 'center',
-    justifyContent: 'flex-end',
-    flexDirection: 'row',
+    justifyContent: 'space-between',
+    flexDirection: 'column',
     width: '80%',
     borderRadius: 20,
     shadowColor: '#000',
@@ -102,10 +106,30 @@ const styles = StyleSheet.create({
     paddingRight: 20,
   },
 
+  top: {
+    display: 'flex',
+    flexDirection: 'row',
+    height: '80%',
+    width: '100%',
+  },
+
   leftSide: {
     flex: 1,
     justifyContent: 'space-around',
     width: '50%',
+    height: '100%',
+    paddingTop: 10,
+    paddingLeft: 0,
+    paddingBottom: 10,
+    paddingRight: 20,
+  },
+
+  rightSide: {
+    flex: 1,
+    justifyContent: 'space-around',
+    alignItems: 'flex-end',
+    width: '50%',
+    height: '100%',
     paddingTop: 10,
     paddingLeft: 0,
     paddingBottom: 10,
@@ -116,6 +140,14 @@ const styles = StyleSheet.create({
     width: 20,
     height: 20,
     marginRight: 5,
+    resizeMode: 'contain',
+  },
+
+  driverImage: {
+    width: 40,
+    height: 40,
+    marginRight: 5,
+    resizeMode: 'contain',
   },
 
   distanceBetweenPoint: {
@@ -140,6 +172,7 @@ const styles = StyleSheet.create({
     paddingLeft: 0,
     paddingBottom: 2,
     paddingRight: 5,
+    marginLeft: 8,
     height: 30,
   },
 
@@ -151,11 +184,12 @@ const styles = StyleSheet.create({
   },
 
   button: {
-    position: 'absolute',
-    bottom: 0,
-    width: '100%',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '115%',
     height: '50px',
-    borderRadius: 25,
+    borderRadius: 15,
     backgroundColor: '#ffa553',
     fontWeight: 600,
   },
